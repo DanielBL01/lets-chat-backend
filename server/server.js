4adefe1e334e86bf4271d58c2c202d03b95b49a7
DanielBL01 <dblee0532@gmail.com>
2021-03-13 15:35:00 -0500

    implement rooms

--- server/server.js
+++ server/server.js
@@ -4,25 +4,31 @@ const io = require('socket.io')(httpServer);
 const port = process.env.PORT || 8000;
 
 const { Message } = require('./db/model/message');
+const { Rooms } = require('./utils/rooms');
+var rooms = new Rooms();
 
 io.on('connection', socket => {
     console.log('A User has connected');
 
+    socket.on('join', room => {
+        socket.join(room);
+        rooms.joinRoom(room, socket.id);
+        var room = rooms.findRoom();
+        console.log(room);
+    });
+
     socket.on('disconnect', () => {
-        console.log('user has disconnected')
-    });
-
-    socket.on('chat message', (msg) => {
-        console.log('message: ' + msg);
-
-        io.emit('chat message', msg);
-    });
+        var id = socket.id;
+        console.log(`User: ${id} has disconnected!`)
+    })
 });
 
+// Test API call to Frontend 
 app.get('/title', (req, res) => {
     res.send('Lets Chat 😎');
 });
 
+// Test MongoDB Connection to store message data
 app.get('/message', async (req, res) => {
     try {
         var message = new Message({message: 'Testing MongoDB', date: Date.now()});
---
+++ server/utils/rooms.js
@@ -0,0 +1,30 @@
+class Rooms {
+    constructor() {
+        this.room = {users: []};
+    }
+
+    joinRoom(socket_id) {
+        this.room.users.push(socket_id);
+    }
+
+    leaveRoom(socket_id) {
+        this.room.users.filter(user => user != socket_id);
+    }
+
+    findRoom() {
+        return this.room;
+    }
+}
+
+module.exports = { Rooms };
+
+/* 
+
+How does Rooms work in Socket.IO?
+
+Rooms in Socket.IO don't need to be created. One is 'created' when a socket joins it. 
+
+This class will keep a list of rooms. Each room will have their own unique IDs and number
+of users in the room to check if its empty, user is waiting for a match or full (users: 2)
+
+*/
